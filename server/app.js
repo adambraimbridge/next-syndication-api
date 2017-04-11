edd88596a84137ad4e3e48f1cf584ef32b77128f
Keith Rogers <git@keithrogers.eu>
2017-04-11 22:04:58 +0100

    Add experimental user endpoint
    So we can grab whether or not the user is in the test group for the new syndication service

--- server/app.js
+++ server/app.js
@@ -1,10 +1,9 @@
 const express = require('@financial-times/n-express');
-
+const cookieParser = require('cookie-parser');
 const app = module.exports = express({
 	systemCode: 'next-syndication-api',
 	withFlags: true
 });
 
-app.get('/__gtg', (req, res) => {
-	res.sendStatus(200);
-});
+app.get('/user', cookieParser(), require('./controllers/user'));
+app.get('/__gtg', (req, res) => res.sendStatus(200));
