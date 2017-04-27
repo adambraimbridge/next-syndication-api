6e0cf9b3c9cdee0f467a66b32fa6acdcba946265
Keith Rogers <git@keithrogers.eu>
2017-04-27 12:16:14 +0100

    Wrap validation in a try/catch for the next commit

--- server/lib/validate-body.js
+++ server/lib/validate-body.js
@@ -1,11 +1,17 @@
 const logger = require('@financial-times/n-logger').default;
 const validateBody = body => {
-	const validBody = body && body.content && typeof body.content.length === 'number';
+	try {
 
-	if (validBody) {
-		return Promise.resolve(body);
-	} else {
-		const err = new Error('Expected a JSON object containing an array property called `content`');
+		const validBody = body.content && typeof body.content.length === 'number';
+
+		if (validBody) {
+			return Promise.resolve(body);
+		} else {
+			throw new Error();
+		}
+	}
+	catch (e) {
+		const err = new Error('Expected a JSON-parseable object containing an array property called `content`');
 		err.statusCode = 400;
 		logger.info('in validate-body lib', { err: err.message, body });
 
