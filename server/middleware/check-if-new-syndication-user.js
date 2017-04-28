9b6b19fdbd61035cb17f282809392c1b15383100
Keith Rogers <git@keithrogers.eu>
2017-04-28 17:21:54 +0100

    Update references to reflect file renaming

--- server/middleware/check-if-new-syndication-user.js
+++ server/middleware/check-if-new-syndication-user.js
@@ -1,12 +1,12 @@
 const logger = require('@financial-times/n-logger').default;
-const grabNewSyndicationUsers = require('../new-syndicators');
+const buildUserArray = require('../lib/build-user-array');
 
 module.exports = (req, res, next) => {
 	const flags = res.locals.flags;
-	const newSyndicationUsers = grabNewSyndicationUsers(flags);
+	const newSyndicationUsers = buildUserArray(flags);
 	const isNewSyndicationUser = newSyndicationUsers.includes(res.locals.userUuid) || flags.syndicationNewOverride;
 
-	logger.info('in check-if-new-syndicator middleware', { isNewSyndicationUser });
+	logger.info('in check-if-new-syndication-user middleware', { isNewSyndicationUser });
 
 	if (isNewSyndicationUser) {
 		res.setHeader('FT-New-Syndication-User', 'true');
