70055b111dc8a8e8e9e8ed5348e64439454da452
constantology <christos@muigui.com>
2017-06-08 11:34:54 +0100

    mo' tests

--- test/server/lib/build-user-array.spec.js
+++ test/server/lib/build-user-array.spec.js
@@ -10,7 +10,46 @@ describe('Build array of syndication API users', () => {
 	});
 
 	it('should include user UUIDs from an env var list of awaiting users, when the syndicationNewUsersAwaiting flag is on', () => {
-		const userArray = buildUserArray({ syndicationNewUsersAwaiting: true });
+		const userArray = buildUserArray({syndicationNewUsersAwaiting: true});
 		expect(userArray).to.deep.equal(['testUserUuid1', 'testUserUuid2', 'testUserUuid3', 'testUserUuid4']);
 	});
+
+	describe('no new users', function () {
+		const NEW_SYNDICATION_USERS = process.env.NEW_SYNDICATION_USERS;
+		const NEW_SYNDICATION_USERS_AWAITING = process.env.NEW_SYNDICATION_USERS_AWAITING;
+
+		after(function () {
+			process.env.NEW_SYNDICATION_USERS = NEW_SYNDICATION_USERS;
+		});
+
+		before(function () {
+			process.env.NEW_SYNDICATION_USERS = '';
+		});
+
+		it('should return an empty array when there is no NEW_SYNDICATION_USERS', () => {
+			const userArray = buildUserArray({});
+			expect(userArray).to.deep.equal([]);
+		});
+
+		it('should return only the user UUIDs from an env var list of awaiting users, when no NEW_SYNDICATION_USERS and the syndicationNewUsersAwaiting flag is on', () => {
+			const userArray = buildUserArray({syndicationNewUsersAwaiting: true});
+			expect(userArray).to.deep.equal(['testUserUuid3', 'testUserUuid4']);
+		});
+
+		describe('no users awaiting', function () {
+			after(function () {
+				process.env.NEW_SYNDICATION_USERS_AWAITING = NEW_SYNDICATION_USERS_AWAITING;
+			});
+
+			before(function () {
+				process.env.NEW_SYNDICATION_USERS_AWAITING = '';
+			});
+
+			it('should return an empty array no NEW_SYNDICATION_USERS and no NEW_SYNDICATION_USERS_AWAITING', () => {
+				const userArray = buildUserArray({syndicationNewUsersAwaiting: true});
+				expect(userArray).to.deep.equal([]);
+			});
+		});
+	});
+
 });
