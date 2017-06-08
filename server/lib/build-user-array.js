f6d487dfa5c1e2fcb7a1d9b3b1763b152183620e
constantology <christos@muigui.com>
2017-06-08 11:34:45 +0100

    altered so code coverage is happy... :^S

--- server/lib/build-user-array.js
+++ server/lib/build-user-array.js
@@ -1,19 +1,19 @@
-const arrayify = (string = '') => {
-	return string.split(',').filter(truthies => truthies);
-};
+'use strict';
 
-module.exports = flags => {
+const buildUserArray = flags => {
 	// UUIDs of existing test users of our new syndication system (this app
 	// plus some bits in n-ui in the syndication folder)
-	const newSyndicationUsersExisting = arrayify(process.env.NEW_SYNDICATION_USERS);
+	const newSyndicationUsersExisting = (process.env.NEW_SYNDICATION_USERS || '').split(',').filter(item => item);
 
 	if (flags.syndicationNewUsersAwaiting) {
 
 		// Add the next batch of folks to the list
-		const newSyndicationUsersAwaiting = arrayify(process.env.NEW_SYNDICATION_USERS_AWAITING);
+		const newSyndicationUsersAwaiting = (process.env.NEW_SYNDICATION_USERS_AWAITING || '').split(',').filter(item => item);
 		return [...newSyndicationUsersExisting, ...newSyndicationUsersAwaiting];
 
 	} else {
 		return newSyndicationUsersExisting;
 	}
 };
+
+module.exports = exports = buildUserArray;
