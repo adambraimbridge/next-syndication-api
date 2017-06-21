7b7c868091fceaf1bff2870d5d58be803d549fb9
constantology <christos@muigui.com>
2017-06-21 17:17:59 +0100

    remove commented out code

--- server/lib/prepare-download-response.js
+++ server/lib/prepare-download-response.js
@@ -1,14 +1,9 @@
 'use strict';
 
-const mime = require('mime-types');
-
 const { DOWNLOAD_ARTICLE_EXTENSION_OVERRIDES } = require('config');
 
 module.exports = exports = (content, res) => {
 	let extension = DOWNLOAD_ARTICLE_EXTENSION_OVERRIDES[content.extension] || content.extension;
 
-//	let contentDisposition = `attachment; filename=${content.fileName}.${extension}`;
-
 	res.attachment(`${content.fileName}.${extension}`);
-//	res.set('content-type', mime.contentType(extension));
 };
