1b8253d3c5e7bae834367af71e51256ba7fd9d20
constantology <christos@muigui.com>
2017-06-22 11:41:30 +0100

    use config

--- server/init.js
+++ server/init.js
@@ -1,19 +1,31 @@
 'use strict';
 
+const path = require('path');
+
 const {default: log} = require('@financial-times/n-logger');
-const port = process.env.PORT || 3255;
+
+const CONFIG = require('config');
+
 const app = require('./app');
+
+const PORT = process.env.PORT || CONFIG.PORT || 3255;
+
+const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
 process.on('uncaughtException', err => {
     log.error(err.stack);
 });
 
-app.listen(port, () => {
-    log.info(`Listening on ${port}`);
+app.listen(PORT, () => {
+    log.info(`Listening on ${PORT}`);
 });
 
 module.exports = app;
 
 process.on('unhandledRejection', (reason, promise) => {
-    log.error(reason, promise);
+    log.warn(`${MODULE_ID} =>`, promise, reason.stack || reason);
+});
+
+process.on('unhandledPromiseRejectionWarning', (reason, promise) => {
+    log.warn(`${MODULE_ID} =>`, promise, reason.stack || reason);
 });
