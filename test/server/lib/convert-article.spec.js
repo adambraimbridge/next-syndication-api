9c45788d03f9803f86cb4a2fac293853ca7c60ae
constantology <christos@muigui.com>
2017-06-23 13:19:18 +0100

    new tests

---
+++ test/server/lib/convert-article.spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const { expect } = require('chai');
+
+const underTest = require('../../../server/lib/convert-article');
+
+const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(path.resolve('./package.json')).name;
+
+describe(MODULE_ID, function () {
+    const content = require('../../fixtures/b59dff10-3f7e-11e7-9d56-25f963e998b2.json');
+
+    [
+        'docx',
+        'html',
+        'plain'
+    ].forEach(targetFormat => {
+        describe(`Converting to: ${targetFormat}`, function () {
+
+            it('returns a Buffer of the converted Article', async function () {
+                const res = await underTest({
+                    source: content.bodyXML__CLEAN,
+                    sourceFormat: 'html',
+                    targetFormat
+                });
+
+                expect(res).to.be.an.instanceOf(Buffer);
+            });
+
+            it('the content should be correct', async function () {
+                const expected = await underTest({
+                    source: content.bodyXML__CLEAN,
+                    sourceFormat: 'html',
+                    targetFormat
+                });
+
+                const actual = fs.readFileSync(path.resolve(`./test/fixtures/article.${targetFormat}`));
+
+                expect(expected.equals(actual)).to.be.true;
+            });
+
+        });
+    });
+});
