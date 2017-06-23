5e9cb537a8669d9c579c5fa4fe7b574c50b442c6
constantology <christos@muigui.com>
2017-06-23 12:03:11 +0100

    throttle queue purging to avoid errors

--- queue/purge.js
+++ queue/purge.js
@@ -10,6 +10,7 @@ const {
     AWS_ACCESS_KEY,
     AWS_REGION = 'eu-west-1',
     AWS_SECRET_ACCESS_KEY,
+    QUEUE_PURGE_THROTTLE_LIMIT_MS: THROTTLE_LIMIT_MS,
     SYNDICATION_DOWNLOAD_SQS_URL: DEFAULT_QUEUE_URL
 } = require('config');
 
@@ -19,11 +20,34 @@ const sqs = new AWS.SQS({
     secretAccessKey: AWS_SECRET_ACCESS_KEY
 });
 
+const THROTTLE_BY_QUEUE = {};
+
 const purgeQueue = async (params = { QueueUrl: DEFAULT_QUEUE_URL }) => {
+    let { QueueUrl } = params;
+
+    if (THROTTLE_BY_QUEUE[QueueUrl]) {
+        const ms = Date.now() - THROTTLE_BY_QUEUE[QueueUrl].now;
+
+        if (THROTTLE_LIMIT_MS > ms) {
+            if (!THROTTLE_BY_QUEUE[QueueUrl].timeoutId) {
+                THROTTLE_BY_QUEUE[QueueUrl].timeoutId = setTimeout(async () => {
+                    clearTimeout(THROTTLE_BY_QUEUE[QueueUrl].timeoutId);
+                    delete THROTTLE_BY_QUEUE[QueueUrl].timeoutId;
+
+                    await purgeQueue(params);
+                }, THROTTLE_LIMIT_MS - ms + 10);
+            }
+
+            return;
+        }
+    }
+
     try {
         let data = await sqs.purgeQueueAsync(params);
 
-        log.info('SyndicationSQSQueuePurgeSuccess', {data, params});
+        log.info('SyndicationSQSQueuePurgeSuccess', JSON.stringify({ data, params }, null, 4));
+
+        THROTTLE_BY_QUEUE[QueueUrl] = { now: Date.now() };
 
         return true;
     }
--- test/queue/purge.spec.js
+++ test/queue/purge.spec.js
@@ -20,7 +20,7 @@ const __proto__ = Object.getPrototypeOf(new AWS.SQS({}));
 
 const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(path.resolve('./package.json')).name;
 
-describe.only(MODULE_ID, function () {
+describe(MODULE_ID, function () {
     before(function () {
         sinon.spy(__proto__, 'purgeQueueAsync');
     });
@@ -37,19 +37,19 @@ describe.only(MODULE_ID, function () {
         expect(success).to.be.true;
     });
 
-//    it('should be throttled', async function () {
-//        let success = await underTest({
-//            QueueUrl: DEFAULT_QUEUE_URL
-//        });
-//
-//        expect(success).to.be.equal(undefined);
-//
-//        success = await underTest({
-//            QueueUrl: DEFAULT_QUEUE_URL
-//        });
-//
-//        expect(success).to.be.equal(undefined);
-//    });
+    it('should be throttled', async function () {
+        let success = await underTest({
+            QueueUrl: DEFAULT_QUEUE_URL
+        });
+
+        expect(success).to.be.equal(undefined);
+
+        success = await underTest({
+            QueueUrl: DEFAULT_QUEUE_URL
+        });
+
+        expect(success).to.be.equal(undefined);
+    });
 
     it('should return false for a failed purge', async function () {
         let success = await underTest({
