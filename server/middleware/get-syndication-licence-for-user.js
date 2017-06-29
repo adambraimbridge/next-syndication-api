d848336aa7a890512e3635fe46577e93a9fd0f3a
constantology <christos@muigui.com>
2017-06-29 13:22:46 +0100

    add middleware to retrieve user's syndication licence

---
+++ server/middleware/get-syndication-licence-for-user.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const path = require('path');
+
+const { default: log } = require('@financial-times/n-logger');
+const fetch = require('n-eager-fetch');
+
+const {
+	ALS_API_KEY,
+	API_KEY_HEADER_NAME,
+	BASE_URI_FT_API,
+	LICENCE_ITEMS_ARRAY_PROPERTY,
+	SYNDICATION_PRODUCT_CODE
+} = require('config');
+
+const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
+
+module.exports = exports = (req, res, next) => {
+	return new Promise((resolve, reject) => {
+		fetch(`${BASE_URI_FT_API}/licences?userid=${res.locals.userUuid}`, { headers: {
+			[API_KEY_HEADER_NAME]: ALS_API_KEY
+		} })
+		.then(licenceRes => {
+			licenceRes.json().then(licences => {
+				const licenceList = licences[LICENCE_ITEMS_ARRAY_PROPERTY];
+
+				const syndicationLicence = licenceList.find(({ products = [], status }) =>
+					status === 'active' && products.find(({ code }) => code === SYNDICATION_PRODUCT_CODE));
+
+				if (!syndicationLicence) {
+					throw new ReferenceError(`${MODULE_ID} => No Syndication Licence found for user#${res.locals.userUuid}`);
+				}
+
+				res.locals.licence = syndicationLicence;
+
+				log.info(`${MODULE_ID}`, syndicationLicence);
+
+				resolve(syndicationLicence);
+
+				next();
+			});
+		})
+		.catch(error => {
+			log.info(`${MODULE_ID}`, { error });
+
+			res.sendStatus(503);
+
+			reject(error);
+		});
+	});
+};
