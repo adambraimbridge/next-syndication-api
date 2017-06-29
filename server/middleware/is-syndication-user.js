1a5d28dcfc368492d706723e1300bb926a4042c9
constantology <christos@muigui.com>
2017-06-29 09:38:37 +0100

    add new middleware to call session api to check if user is a syndication user

---
+++ server/middleware/is-syndication-user.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const path = require('path');
+
+const { default: log } = require('@financial-times/n-logger');
+const fetch = require('n-eager-fetch');
+
+const {
+	SESSION_PRODUCTS_PATH,
+	SESSION_URI,
+	SYNDICATION_PRODUCT_CODE
+} = require('config');
+
+const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
+
+module.exports = (req, res, next) => {
+	const headers = JSON.parse(JSON.stringify(req.headers));
+
+	delete headers.host;
+
+	let isSyndicationUser = false;
+
+	return new Promise((resolve, reject) => {
+		fetch(`${SESSION_URI}${SESSION_PRODUCTS_PATH}`, { headers })
+		.then(sessionRes => {
+			if (!sessionRes.ok) {
+				log.info(`${MODULE_ID}`, { isSyndicationUser: false });
+
+				res.sendStatus(401);
+
+				return resolve({ isSyndicationUser });
+			}
+
+			sessionRes.json().then(session => {
+				isSyndicationUser = session.uuid === res.locals.userUuid
+								&& session.products.split(',').includes(SYNDICATION_PRODUCT_CODE);
+
+				log.info(`${MODULE_ID}`, { isSyndicationUser });
+
+				if (isSyndicationUser !== true) {
+					res.sendStatus(401);
+
+					resolve({ isSyndicationUser });
+				}
+				else {
+					resolve({ isSyndicationUser });
+					next();
+				}
+			});
+		})
+		.catch(error => {
+			log.info(`${MODULE_ID}`, { error });
+
+			res.sendStatus(503);
+
+			reject(error);
+		});
+	});
+};
---
+++ test/server/middleware/is-syndication-user.spec.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const path = require('path');
+
+const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+const nock = require('nock');
+const proxyquire = require('proxyquire');
+
+chai.use(sinonChai);
+
+const {
+	SESSION_PRODUCTS_PATH,
+	SESSION_URI,
+	SYNDICATION_PRODUCT_CODE
+} = require('config');
+
+const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(path.resolve('./package.json')).name;
+
+describe(MODULE_ID, function () {
+	let sandbox;
+	let mocks;
+	let stubs;
+	let underTest;
+
+	beforeEach(function () {
+		sandbox = sinon.sandbox.create();
+		mocks = {
+			req: {
+				cookies: {
+					FTSession: '123'
+				},
+				headers: {}
+			},
+			res: {
+				locals: {},
+				sendStatus: sandbox.stub()
+			}
+		};
+		stubs = {
+			logger: {
+				default: {
+					info: sandbox.stub()
+				}
+			},
+			next: sandbox.stub()
+		};
+
+		underTest = proxyquire('../../../server/middleware/is-syndication-user', {
+			'@financial-times/n-logger': stubs.logger
+		});
+	});
+
+	afterEach(function () {
+		sandbox.restore();
+	});
+
+//	it('should send an unauthorised status code if the session service returns a 404', function () {
+//		nock(SESSION_URI)
+//			.get(SESSION_PRODUCTS_PATH)
+//			.reply(404, 'Not Found', {});
+//
+//		return underTest(mocks.req, mocks.res, stubs.next).then(() => {
+//			expect(mocks.res.sendStatus).to.have.been.calledWith(401);
+//
+//			expect(stubs.next).not.to.have.been.called;
+//		});
+//
+//	});
+
+	it(`should send an unauthorised status code if the session service products does NOT contain ${SYNDICATION_PRODUCT_CODE}`, function () {
+		nock(SESSION_URI)
+			.get(SESSION_PRODUCTS_PATH)
+			.reply(200, { uuid: 'abc', products: 'Tools,P0,P1,P2' }, {});
+
+		return underTest(mocks.req, mocks.res, stubs.next).then(() => {
+			expect(mocks.res.sendStatus).to.have.been.calledWith(401);
+			expect(stubs.next).not.to.have.been.called;
+		});
+	});
+
+	it('should send an unauthorised status code if the session service UUID does not match the session UUID', function () {
+		mocks.res.locals.userUuid = 'xyz';
+
+		nock(SESSION_URI)
+			.get(SESSION_PRODUCTS_PATH)
+			.reply(200, { uuid: 'abc', products: 'Tools,S1,P0,P1,P2' }, {});
+
+		return underTest(mocks.req, mocks.res, stubs.next).then(() => {
+			expect(mocks.res.sendStatus).to.have.been.calledWith(401);
+			expect(stubs.next).not.to.have.been.called;
+		});
+	});
+
+	it(`should continue on if the session service products does contain ${SYNDICATION_PRODUCT_CODE} and the session service UUID matches the session UUID`, function () {
+		mocks.res.locals.userUuid = 'abc';
+
+		nock(SESSION_URI)
+			.get(SESSION_PRODUCTS_PATH)
+			.reply(200, { uuid: 'abc', products: 'Tools,S1,P0,P1,P2' }, {});
+
+		return underTest(mocks.req, mocks.res, stubs.next).then(() => {
+			expect(mocks.res.sendStatus).to.not.have.been.called;
+			expect(stubs.next).to.have.been.called;
+		});
+	});
+
+});
