00194ceff77a384bdbb78d1515fe9ca1fc066949
constantology <christos@muigui.com>
2017-06-30 14:18:21 +0100

    remove temp dir after tests run

--- test/server/lib/bundle-content.spec.js
+++ test/server/lib/bundle-content.spec.js
@@ -30,6 +30,10 @@ const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(p
 mkdir('-p', path.resolve(TEMP_FILES_DIRECTORY));
 
 describe(MODULE_ID, function () {
+	after(function() {
+		rm('-rf', path.resolve(TEMP_FILES_DIRECTORY));
+	});
+
 	describe('With a transcript and captions', function () {
 		const CONTENT_ID = 'd7bf1822-ec58-4a8e-a669-5cbcc0d6a1b2';
 
