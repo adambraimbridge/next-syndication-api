2bd54dc25cc6afb95dafcdaf0833b8a551b2b8ad
constantology <christos@muigui.com>
2017-07-12 11:26:48 +0100

    fix linting

--- server/helpers/skip-checks.js
+++ server/helpers/skip-checks.js
@@ -3,4 +3,4 @@
 const flagIsOn = require('./flag-is-on');
 
 module.exports = exports = (flags) =>
-    process.env.NODE_ENV !== 'production' && flagIsOn(flags.syndicationRedux);
+	process.env.NODE_ENV !== 'production' && flagIsOn(flags.syndicationRedux);
