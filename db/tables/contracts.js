9613fe8fb64b5cee8d9e79aef8573916df610218
constantology <christos@muigui.com>
2017-07-13 16:50:29 +0100

    initial schemas for tables

---
+++ db/tables/contracts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = exports = {
+    TableName : 'ft-next-syndication-contracts',
+    KeySchema: [
+        { AttributeName: 'contract_number', KeyType: 'HASH'}
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'contract_number', AttributeType: 'S' }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
---
+++ db/tables/history.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = exports = {
+    TableName : 'ft-next-syndication-history',
+    KeySchema: [
+        { AttributeName: '_id', KeyType: 'HASH'}
+    ],
+    AttributeDefinitions: [
+        { AttributeName: '_id', AttributeType: 'S' }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
