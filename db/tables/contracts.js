b551e12d61f3f6eb768d6d7c6e33f7ec7d21c615
constantology <christos@muigui.com>
2017-07-13 16:52:11 +0100

    fix linting

--- db/tables/contracts.js
+++ db/tables/contracts.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports = exports = {
-    TableName : 'ft-next-syndication-contracts',
-    KeySchema: [
-        { AttributeName: 'contract_number', KeyType: 'HASH'}
-    ],
-    AttributeDefinitions: [
-        { AttributeName: 'contract_number', AttributeType: 'S' }
-    ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
-    }
+	TableName: 'ft-next-syndication-contracts',
+	KeySchema: [
+		{ AttributeName: 'contract_number', KeyType: 'HASH' }
+	],
+	AttributeDefinitions: [
+		{ AttributeName: 'contract_number', AttributeType: 'S' }
+	],
+	ProvisionedThroughput: {
+		ReadCapacityUnits: 10,
+		WriteCapacityUnits: 10
+	}
 };
