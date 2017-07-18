bd27f513dce2d313f3e9cab6df5039e859cc0877
constantology <christos@muigui.com>
2017-07-18 09:29:34 +0100

    changed this so it doesn't conflict with SQS credentials

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,24 @@
 
 require('./promisify');
 
-const AWS = require('./aws');
+const AWS = require('aws-sdk');
 
-const db = new AWS.DynamoDB();
+const {
+	AWS_ACCESS_KEY,
+	AWS_REGION = 'eu-west-1',
+	AWS_SECRET_ACCESS_KEY,
+	DEFAULT_DB_ENDPOINT
+} = require('config');
 
-const client = new AWS.DynamoDB.DocumentClient();
+const db = new AWS.DynamoDB({
+	accessKeyId: AWS_ACCESS_KEY,
+	endpoint: DEFAULT_DB_ENDPOINT,
+	region: AWS_REGION,
+	secretAccessKey: AWS_SECRET_ACCESS_KEY
+});
+
+const client = new AWS.DynamoDB.DocumentClient({
+	service: db
+});
 
 module.exports = exports = { client, db };
