56367789f17a167ca7d5f20c5aa8cecaa15fee1c
constantology <christos@muigui.com>
2017-07-18 09:36:57 +0100

    updated to persist items to the db and remove them from the queue if successful

--- sync/index.js
+++ sync/index.js
@@ -8,16 +8,31 @@ const {
 	SYNDICATION_DOWNLOAD_SQS_URL: DEFAULT_QUEUE_URL
 } = require('config');
 
+require('../db/init');
+
 const QueueSubscriber = require('../queue/subscriber');
 const sleep = require('../server/helpers/sleep');
+
+const persist = require('./persist');
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
 (async () => {
 	let subscriber = new QueueSubscriber({ queue_url: DEFAULT_QUEUE_URL });
 
-	subscriber.start(async (messages) => {
-		log.info(`${MODULE_ID} => `, messages);
+	subscriber.start(async (event, message) => {
+		try {
+			log.debug(`${MODULE_ID} RECEIVED => `, event);
+
+			let res = await persist(event);
+
+			log.debug(`${MODULE_ID} PERSISTED => `, res);
+
+			await subscriber.ack(message);
+		}
+		catch (e) {
+			log.error(`${MODULE_ID} => `, e);
+		}
 	});
 
 	while (true) {
