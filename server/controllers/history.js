2faeb156ce822fc336dfc6f08ae6cd68389cd02a
constantology <christos@muigui.com>
2017-07-18 11:38:18 +0100

    sort items in descending order by `time:Date`; add support for filtering by `type:enum(downloads,saved,undefined)`; add support for showing only items current user has actioned

--- server/controllers/history.js
+++ server/controllers/history.js
@@ -11,19 +11,47 @@ const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolv
 
 module.exports = exports = async (req, res, next) => {
 	try {
+		let FilterExpression = 'licence_id = :licence_id';
+		const ExpressionAttributeValues = {
+			':licence_id': res.locals.licence.id
+		};
+
+		if (req.query.show === 'mine') {
+			FilterExpression += ' and user_id = :user_id';
+
+			ExpressionAttributeValues[':user_id'] = res.locals.userUuid;
+		}
+
 		let response = await client.scanAsync({
 			TableName: HistoryTable.TableName,
-			FilterExpression: 'licence_id = :licence_id',
-			ExpressionAttributeValues: {
-				':licence_id': res.locals.licence.id
-			}
+			FilterExpression,
+			ExpressionAttributeValues
 		});
 
-		if (response && Array.isArray(response.Items)) {
-			if (response.Items.length) {
+		if (response) {
+			if (response.Count > 0) {
 				res.status(200);
 
-				res.json(response.Items);
+				// Sort items in descending order by `time:Date`
+				// This is a schwartzian transform, if you don't know what that is
+				// and/or why it's being used, I suggest you look it up.
+				// DO NOT try and "simplify" — and thereby slow down the sort — if you do not understand what's going on.
+				let items = response.Items.map(item => {
+						return [+(new Date(item.time)), item];
+					})
+					.sort(([a], [b]) => b - a)
+					.map(([, item]) => item);
+
+				switch (req.query.type) {
+					case 'downloads':
+						items = items.filter(item => item.state !== 'save');
+						break;
+					case 'saved':
+						items = items.filter(item => item.state === 'save');
+						break;
+				}
+
+				res.json(items);
 			}
 			else {
 				res.sendStatus(204);
