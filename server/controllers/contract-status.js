f0747ddb0a29a6ec0eca3c309dc0be29a4dcf663
constantology <christos@muigui.com>
2017-07-19 11:55:44 +0100

    move logic to get a contract by ID to its own file so it can be used in new endpoints

--- server/controllers/contract-status.js
+++ server/controllers/contract-status.js
@@ -5,79 +5,37 @@ const path = require('path');
 
 const { default: log } = require('@financial-times/n-logger');
 
-const nforce = require('nforce');
+//const {
+//	SALESFORCE: {
+//		ENVIRONMENT: SALESFORCE_ENVIRONMENT,
+//		TEST_CONTRACT: SALESFORCE_TEST_CONTRACT
+//	}
+//} = require('config');
 
-const {
-	SALESFORCE: {
-		API_VERSION: SALESFORCE_API_VERSION,
-		BASE_URI: SALESFORCE_URI,
-		CALLBACK_URI: SALESFORCE_CALLBACK_URI,
-		CLIENT_ID: SALESFORCE_CLIENT_ID,
-		CLIENT_SECRET: SALESFORCE_CLIENT_SECRET,
-		CONNECTION_MODE: SALESFORCE_CONNECTION_MODE,
-		ENVIRONMENT: SALESFORCE_ENVIRONMENT,
-		PASSWORD: SALESFORCE_PASSWORD,
-		USERNAME: SALESFORCE_USERNAME,
-		TEST_CONTRACT: SALESFORCE_TEST_CONTRACT
-	}
-} = require('config');
-
-//const flagIsOn = require('../helpers/flag-is-on');
+const getContractByID = require('../lib/get-contract-by-id');
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
 module.exports = exports = async (req, res, next) => {
 	try {
-
-		const org = nforce.createConnection({
-			apiVersion: SALESFORCE_API_VERSION,
-			autoRefresh: true,
-			clientId: SALESFORCE_CLIENT_ID,
-			clientSecret: SALESFORCE_CLIENT_SECRET,
-			environment: SALESFORCE_ENVIRONMENT,
-			mode: SALESFORCE_CONNECTION_MODE,
-			redirectUri: SALESFORCE_CALLBACK_URI
-		});
-
-		const oauth = await org.authenticate({
-			username: SALESFORCE_USERNAME,
-			password: SALESFORCE_PASSWORD
-		});
-
-		log.debug(`${MODULE_ID} | SALESFORCE OAUTH => `, oauth);
-
-		const SALESFORCE_CONTRACT_ID = SALESFORCE_ENVIRONMENT === 'sandbox'
+		const SALESFORCE_CONTRACT_ID = /*SALESFORCE_ENVIRONMENT === 'sandbox'
 									? SALESFORCE_TEST_CONTRACT
-									: res.locals.syndicationContractID;
+									:*/ res.locals.syndicationContractID;
 
-		log.debug(`${MODULE_ID} | SALESFORCE CONTRACT URI => `, `${SALESFORCE_URI}/${SALESFORCE_CONTRACT_ID}`);
+		const contract = await getContractByID(SALESFORCE_CONTRACT_ID);
 
-		let apexRes = await org.apexRest({
-			uri:`${SALESFORCE_URI}/${SALESFORCE_CONTRACT_ID}`,
-			method: 'GET',
-			oauth: oauth
-		});
+		if (contract.success === true) {
+			res.status(200);
+			res.json(contract);
 
-		log.debug(`${MODULE_ID} | APEX RESPONSE => `, apexRes);
-
-		if (apexRes) {
-			if (apexRes.success === true) {
-				res.status(200);
-				res.json(apexRes);
-
-				next();
-			}
-			else {
-				res.status(400);
-				res.json({
-					error: apexRes.errorMessage
-				});
-			}
+			next();
 		}
 		else {
-			res.sendStatus(503);
+			res.status(400);
+			res.json({
+				error: contract.errorMessage
+			});
 		}
-
 	}
 	catch (error) {
 		log.error(`${MODULE_ID}`, {
