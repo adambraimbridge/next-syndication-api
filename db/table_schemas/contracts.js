cf691f343928123d5a861b15b6cf58f23a165043
constantology <christos@muigui.com>
2017-07-19 14:40:39 +0100

    updated to include assets properties and attribute aliases

--- db/table_schemas/contracts.js
+++ db/table_schemas/contracts.js
@@ -3,18 +3,31 @@
 module.exports = exports = {
 	TableName: 'ft-next-syndication-contracts',
 	AttributeDefinitions: [
-		{ AttributeName: 'contract_number', AttributeType: 'S' },
-		{ AttributeName: 'contract_ends', AttributeType: 'S' },
-		{ AttributeName: 'contract_starts', AttributeType: 'S' },
-		{ AttributeName: 'contributor_content', AttributeType: 'N' },
-		{ AttributeName: 'client_publications', AttributeType: 'S' },
-		{ AttributeName: 'client_website', AttributeType: 'S' },
-		{ AttributeName: 'licencee_name', AttributeType: 'S' },
-		{ AttributeName: 'limit_article', AttributeType: 'N' },
-		{ AttributeName: 'limit_podcast', AttributeType: 'N' },
-		{ AttributeName: 'limit_video', AttributeType: 'N' },
-		{ AttributeName: 'owner_name', AttributeType: 'S' },
-		{ AttributeName: 'owner_email', AttributeType: 'S' },
-		{ AttributeName: 'assets', AttributeType: 'S' }
+		{ AttributeAlias: 'contract_number', AttributeName: 'contractNumber', AttributeType: 'S' },
+		{ AttributeAlias: 'contract_ends', AttributeName: 'endDate', AttributeType: 'S' },
+		{ AttributeAlias: 'contract_starts', AttributeName: 'startDate', AttributeType: 'S' },
+		{ AttributeAlias: 'contributor_content', AttributeName: 'contributor', AttributeType: 'BOOL' },
+		{ AttributeAlias: 'client_publications', AttributeName: 'clientPublications', AttributeType: 'S' },
+		{ AttributeAlias: 'client_website', AttributeName: 'clientWebsite', AttributeType: 'S' },
+		{ AttributeAlias: 'licencee_name', AttributeName: 'licenceeName', AttributeType: 'S' },
+		{ AttributeAlias: 'limit_article', AttributeName: 'articleLimit', AttributeType: 'N' },
+		{ AttributeAlias: 'limit_podcast', AttributeName: 'podcastLimit', AttributeType: 'N' },
+		{ AttributeAlias: 'limit_video', AttributeName: 'videoLimit', AttributeType: 'N' },
+		{ AttributeAlias: 'owner_name', AttributeName: 'ownerName', AttributeType: 'S' },
+		{ AttributeAlias: 'owner_email', AttributeName: 'ownerEmail', AttributeType: 'S' },
+		{ AttributeAlias: 'last_updated', AttributeName: 'last_updated', AttributeType: 'S' },
+		{
+			AttributeAlias: 'assets', AttributeName: 'assets', AttributeType: 'L',
+			AttributeDefinitions: [
+				{ AttributeAlias: 'product', AttributeName: 'productName', AttributeType: 'S' },
+				{ AttributeAlias: 'print_usage_period', AttributeName: 'maxPermittedPrintUsagePeriod', AttributeType: 'S' },
+				{ AttributeAlias: 'print_usage_limit', AttributeName: 'maxPermittedPrintUsage', AttributeType: 'N' },
+				{ AttributeAlias: 'online_usage_period', AttributeName: 'maxPermittedOnlineUsagePeriod', AttributeType: 'S' },
+				{ AttributeAlias: 'online_usage_limit', AttributeName: 'maxPermittedOnlineUsage', AttributeType: 'N' },
+				{ AttributeAlias: 'embargo_period', AttributeName: 'embargoPeriod', AttributeType: 'N' },
+				{ AttributeAlias: 'content', AttributeName: 'contentSet', AttributeType: 'S' },
+				{ AttributeAlias: 'asset', AttributeName: 'assetName', AttributeType: 'S' }
+			]
+		}
 	]
 };
