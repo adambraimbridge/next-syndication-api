e07b7d605e95e5d3f549fdc3ed27c4f709e1d8cc
constantology <christos@muigui.com>
2017-07-19 14:41:27 +0100

    updated to allow for recursive resolution of properties and added unit tests

--- db/toPutItem.js
+++ db/toPutItem.js
@@ -1,20 +1,53 @@
 'use strict';
 
 module.exports = exports = (data, schema) => {
-	let item = {
-		Item: {},
+	return {
+		Item: assignProperties(data, schema.AttributeDefinitions),
 		TableName: schema.TableName
 	};
+};
 
-	schema.AttributeDefinitions.reduce((acc, def) => {
+function assignProperties(data, schema, item = {}, simplify) {
+	schema.reduce((acc, def) => {
 		if (Object.prototype.hasOwnProperty.call(data, def.AttributeName)) {
-			acc[def.AttributeAlias || def.AttributeName] = {
-				[def.AttributeType]: data[def.AttributeName]
-			};
+			const ATTRIBUTE_ID = def.AttributeAlias || def.AttributeName;
+
+			let val = data[def.AttributeName];
+
+			acc[ATTRIBUTE_ID] = {};
+
+			if (Array.isArray(def.AttributeDefinitions)) {
+				switch (def.AttributeType) {
+					case 'L':
+						if (!Array.isArray(val)) {
+							val = [val];
+						}
+
+						acc[ATTRIBUTE_ID][def.AttributeType] = val.map(raw => {
+							return {
+								M: assignProperties(raw, def.AttributeDefinitions)
+							};
+						});
+
+						break;
+					case 'M':
+						acc[ATTRIBUTE_ID][def.AttributeType] = assignProperties(val, def.AttributeDefinitions);
+
+						break;
+				}
+			}
+			else {
+				if (simplify === true) {
+					acc[ATTRIBUTE_ID] = val;
+				}
+				else {
+					acc[ATTRIBUTE_ID][def.AttributeType] = def.AttributeType === 'N' ? String(val) : val;
+				}
+			}
 		}
 
 		return acc;
-	}, item.Item);
+	}, item);
 
 	return item;
-};
+}
