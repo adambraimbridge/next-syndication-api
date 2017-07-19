94a9931a5aa1e9aa55c3cf832900fbc7812fc147
constantology <christos@muigui.com>
2017-07-19 14:38:44 +0100

    updated to try getting contract from DB first, or salesforce if it does not exist and refreshing contract data once a day

--- server/controllers/contract-status.js
+++ server/controllers/contract-status.js
@@ -5,13 +5,18 @@ const path = require('path');
 
 const { default: log } = require('@financial-times/n-logger');
 
-//const {
-//	SALESFORCE: {
+const {
+	SALESFORCE: {
 //		ENVIRONMENT: SALESFORCE_ENVIRONMENT,
+		REFRESH_CONTRACT_PERIOD: SALESFORCE_REFRESH_CONTRACT_PERIOD
 //		TEST_CONTRACT: SALESFORCE_TEST_CONTRACT
-//	}
-//} = require('config');
+	}
+} = require('config');
 
+const ContractsSchema = require('../../db/table_schemas/contracts');
+const ContractsTable = require('../../db/tables/contracts');
+const { db, client } = require('../../db/connect');
+const toPutItem = require('../../db/toPutItem');
 const getContractByID = require('../lib/get-contract-by-id');
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
@@ -22,13 +27,46 @@ module.exports = exports = async (req, res, next) => {
 									? SALESFORCE_TEST_CONTRACT
 									:*/ res.locals.syndicationContractID;
 
+		const dbItem = await client.getAsync({
+			TableName: ContractsTable.TableName,
+			Key: {
+				[ContractsTable.AttributeDefinitions[0].AttributeName]: SALESFORCE_CONTRACT_ID
+			}
+		});
+
+		if (dbItem.Item && dbItem.Item[ContractsTable.AttributeDefinitions[0].AttributeName] === SALESFORCE_CONTRACT_ID) {
+			const item = dbItem.Item;
+			let last_updated = Date.now() - +(new Date(item.last_updated));
+
+			if (last_updated < SALESFORCE_REFRESH_CONTRACT_PERIOD) {
+				log.debug(`${MODULE_ID} | Using DB version of contract#${SALESFORCE_CONTRACT_ID}`, dbItem);
+
+				res.status(200);
+
+				res.json(item);
+
+				next();
+
+				return;
+			}
+		}
+
 		const contract = await getContractByID(SALESFORCE_CONTRACT_ID);
 
 		if (contract.success === true) {
+			contract.last_updated = (new Date()).toJSON();
+
 			res.status(200);
+
 			res.json(contract);
 
 			next();
+
+			const dbItem = toPutItem(contract, ContractsSchema);
+
+			const dbRes = await db.putItemAsync(dbItem);
+
+			log.debug(`${MODULE_ID} | Persisted contract#${SALESFORCE_CONTRACT_ID} to DB`, { dbItem, dbRes });
 		}
 		else {
 			res.status(400);
