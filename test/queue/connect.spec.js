057e4ad6fc0ec4daeae97c4b1abda77e9c451369
constantology <christos@muigui.com>
2017-07-19 15:58:59 +0100

    unit tests

---
+++ test/queue/connect.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+
+const { expect } = require('chai');
+
+const AWS = require('aws-sdk');
+
+const {
+	AWS_ACCESS_KEY,
+	AWS_REGION,
+	AWS_SECRET_ACCESS_KEY
+} = require('config');
+
+const underTest = require('../../queue/connect');
+
+const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(path.resolve('./package.json')).name;
+
+describe(MODULE_ID, function () {
+	it('should be an instance of AWS.SQS', function () {
+		expect(underTest).to.be.an.instanceOf(AWS.SQS);
+	});
+
+	it('should have the correct credentials', function () {
+		expect(underTest.config.accessKeyId).to.equal(AWS_ACCESS_KEY);
+
+		expect(underTest.config.region).to.equal(AWS_REGION);
+
+		expect(underTest.config.secretAccessKey).to.equal(AWS_SECRET_ACCESS_KEY);
+	});
+});
