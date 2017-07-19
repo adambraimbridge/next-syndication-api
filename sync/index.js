192e5305c9863c51246c291a8358d4779bbebea7
constantology <christos@muigui.com>
2017-07-19 15:31:58 +0100

    move callback to its own file so it's easier to test

--- sync/index.js
+++ sync/index.js
@@ -8,31 +8,16 @@ const {
 	SYNDICATION_DOWNLOAD_SQS_URL: DEFAULT_QUEUE_URL
 } = require('config');
 
-//require('../db/init');
-
 const QueueSubscriber = require('../queue/subscriber');
 
-const persist = require('./persist');
+const callback = require('./callback');
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
 (async () => {
 	let subscriber = new QueueSubscriber({ queue_url: DEFAULT_QUEUE_URL });
 
-	subscriber.start(async (event, message) => {
-		try {
-			log.debug(`${MODULE_ID} RECEIVED => `, event);
-
-			let res = await persist(event);
-
-			log.debug(`${MODULE_ID} PERSISTED => `, res);
-
-			await subscriber.ack(message);
-		}
-		catch (e) {
-			log.error(`${MODULE_ID} => `, e);
-		}
-	});
+	subscriber.start(callback);
 })();
 
 process.on('uncaughtException', err => {
