5879290083129646e503d0c6dc8aa6bfdda1bfe2
constantology <christos@muigui.com>
2017-08-07 16:16:04 +0100

    check using SAVED_STATE_MAP

--- server/lib/resolve/saved.js
+++ server/lib/resolve/saved.js
@@ -1,4 +1,6 @@
 'use strict';
 
-module.exports = exports = (val, prop, item, saved) =>
-	Object.prototype.toString.call(saved) === '[object Object]';
+const { SAVED_STATE_MAP } = require('config');
+
+module.exports = exports = (val, prop, item, existing) =>
+	Object.prototype.toString.call(existing) === '[object Object]' && existing.item_state in SAVED_STATE_MAP;
