add7519435491ace697f1c74253c0ba7fd2bc1d3
constantology <christos@muigui.com>
2017-08-07 16:15:48 +0100

    use CONTENT_TYPE_ALIAS  🐿 v2.5.13

--- server/lib/resolve/type.js
+++ server/lib/resolve/type.js
@@ -3,4 +3,10 @@
 const path = require('path');
 const url = require('url');
 
-module.exports = exports = item => String(path.basename((url.parse(item)).pathname)).toLowerCase();
+const { CONTENT_TYPE_ALIAS } = require('config');
+
+module.exports = exports = val => {
+	const type = String(path.basename((url.parse(val)).pathname)).toLowerCase();
+
+	return CONTENT_TYPE_ALIAS[type] || type;
+};
