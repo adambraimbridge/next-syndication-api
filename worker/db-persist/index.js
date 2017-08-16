565dffe49a7f2e7b7d9529534ac0085852b4a512
constantology <christos@muigui.com>
2017-08-16 11:29:55 +0100

    switched from DynamoDB to PostgreSQL

--- worker/db-persist/index.js
+++ worker/db-persist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const MessageQueueEvent = require('../../queue/message-queue-event');
-const SchemaJobV1 = require('../../schema/job-v1.json');
+//const MessageQueueEvent = require('../../queue/message-queue-event');
+//const SchemaJobV1 = require('../../schema/job-v1.json');
 
 const worker = require('../index');
 
@@ -9,22 +9,23 @@ const callback = require('./callback');
 
 const event_type = 'db.persist';
 
-const subscriber = worker({ callback, event_type });
+//const subscriber = worker({ callback, event_type });
+worker({ callback, event_type });
 
 // once we've processed all messages, we'll fire off another event to aggregate the download counts
-subscriber.on('complete', async (count, response) => {
-	const {
-		contract_id,
-		licence_id
-	} = response.Messages[0].data;
-
-	const message = new MessageQueueEvent({
-		event: {
-			contract_id,
-			licence_id
-		},
-		schema: SchemaJobV1
-	});
-
-	process.nextTick(async () => await message.publish());
-});
+//subscriber.on('complete', async (count, response) => {
+//	const {
+//		contract_id,
+//		licence_id
+//	} = response.Messages[0].data;
+//
+//	const message = new MessageQueueEvent({
+//		event: {
+//			contract_id,
+//			licence_id
+//		},
+//		schema: SchemaJobV1
+//	});
+//
+//	process.nextTick(async () => await message.publish());
+//});
