b351b3189c888aeba09823daeed5bf2664425e89
constantology <christos@muigui.com>
2017-08-16 11:00:09 +0100

    conveniences for mapping JSON structures to DB table column names

---
+++ db/column_mappings/users.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = exports = {
+	items: [{
+		cite: 'id',
+		id: 'user_id'
+	}, {
+		cite: 'firstName',
+		id: 'first_name'
+	}, {
+		cite: 'lastName',
+		id: 'surname'
+	}]
+};
