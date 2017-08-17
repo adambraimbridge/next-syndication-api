ef9edeeb29aee0a9d1bf70da21d20074ba2953b9
constantology <christos@muigui.com>
2017-08-17 08:48:57 +0100

    use proxyquire to stub out pg-promise inside massive

--- test/fixtures/massive.js
+++ test/fixtures/massive.js
@@ -1,23 +1,42 @@
 'use strict';
 
+const proxyquire = require('proxyquire').noPreserveCache();
 const sinon = require('sinon');
 
-const massiveDatabase = require('massive/lib/database');
+const pg = require('../../db/pg');
 
-const massiveDatabase__proto__ = Object.getPrototypeOf(massiveDatabase);
+pg().then(() => {});
 
 module.exports = exports = function () {
+	const massiveDatabase = proxyquire('massive/lib/database', {
+		'pg-promise': sinon.stub().returns(sinon.stub().returns({
+			query: sinon.stub(),
+			$config: { promise: Promise }
+		})),
+		'@noCallThru': true
+	});
+
+	const massiveDatabase__proto__ = massiveDatabase.prototype;
+
+	function initDB(runResolves) {
+		let db = Object.create(massiveDatabase__proto__);
+//		massiveDatabase__proto__.constructor.call(db);
+
+		db.run = sinon.stub().resolves(runResolves);
+
+		return db;
+	}
+
 	beforeEach(function () {
-		massiveDatabase__proto__.constructor = sinon.stub().returns(Object.create(massiveDatabase__proto__));
-		massiveDatabase__proto__.query = sinon.stub();
-		massiveDatabase__proto__.run = sinon.stub();
+		sinon.stub(massiveDatabase__proto__, 'constructor').returns(initDB());
+		sinon.stub(massiveDatabase__proto__, 'query');
+		sinon.stub(massiveDatabase__proto__, 'run');
 	});
 
 	afterEach(function () {
-		massiveDatabase__proto__.constructor.release();
-		massiveDatabase__proto__.query.release();
-		massiveDatabase__proto__.run.release();
+		massiveDatabase__proto__.constructor.restore();
+		massiveDatabase__proto__.query.restore();
 	});
 
-	return massiveDatabase__proto__;
+	return { initDB, massiveDatabase__proto__ };
 };
