f9e9d86dab78867d75dbbba8455805227b832185
constantology <christos@muigui.com>
2017-08-22 16:00:27 +0100

    added messageCode to returned items

---
+++ server/lib/resolve/messageCode.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const path = require('path');
+
+const { default: log } = require('@financial-times/n-logger');
+
+const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
+
+module.exports = exports = (item, contract) => {
+	try {
+		for (let [code, rule] of ITER_MESSAGE_CODE_RULES) {
+			if (rule(item, contract)) {
+				item.messageCode = code;
+
+				break;
+			}
+		}
+
+		if (!item.messageCode) {
+			item.messageCode = ERROR_CODE;
+		}
+	}
+	catch (e) {
+		log.error(`${MODULE_ID} => MessageCodeError`, e.stack);
+
+		item.messageCode = ERROR_CODE;
+	}
+
+	return item.messageCode;
+};
+
+const ERROR_CODE = 'MSG_5000';
+
+const MESSAGE_CODE_RULES = exports.MESSAGE_CODE_RULES = {
+	MSG_2000: (item, contract) =>
+		(item.canBeSyndicated === 'yes' && !MESSAGE_CODE_RULES.MSG_2100(item, contract)),
+	MSG_2100: (item) =>
+		(item.downloaded === true),
+	MSG_2200: (item) =>
+		(item.canBeSyndicated === 'verify'),
+	MSG_2300: (item) =>
+		(item.canBeSyndicated === 'withContributorPayment'),
+	MSG_4000: (item) =>
+		(item.canBeSyndicated === 'no' || item.canBeSyndicated === null),
+	MSG_4100: (item) =>
+		(item.canDownload === -1),
+	MSG_4200: (item) =>
+		(item.canDownload === 0)
+};
+
+const ITER_MESSAGE_CODE_RULES = Object.entries(MESSAGE_CODE_RULES);
---
+++ test/server/lib/resolve/messsageCode.spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* eslint-disable */
+
+const path = require('path');
+
+const { expect } = require('chai');
+
+const underTest = require('../../../../server/lib/resolve/messageCode');
+
+const {
+	TEST: { FIXTURES_DIRECTORY }
+} = require('config');
+
+const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(path.resolve('./package.json')).name;
+
+describe(MODULE_ID, function () {
+	const contractResponse = require(path.resolve(`${FIXTURES_DIRECTORY}/contractResponse.json`));
+
+	const messageItemMap = {
+		MSG_2000: { canBeSyndicated: 'yes', downloaded: false },
+		MSG_2100: { canBeSyndicated: 'yes', downloaded: true },
+		MSG_2200: { canBeSyndicated: 'verify' },
+		MSG_2300: { canBeSyndicated: 'withContributorPayment' },
+		MSG_4000: { canBeSyndicated: 'no' },
+		MSG_4100: { canDownload: -1 },
+		MSG_4200: { canDownload: 0 },
+		MSG_5000: {}
+	};
+
+	for (let [code, item] of Object.entries(messageItemMap)) {
+		it(`message code: ${code}`, function () {
+			expect(underTest(item, contractResponse)).to.equal(code);
+		});
+	}
+});
