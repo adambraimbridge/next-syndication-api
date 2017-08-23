2536a56b73678e606230956be51edb64db4f34aa
constantology <christos@muigui.com>
2017-08-23 17:55:33 +0100

    initial implementation for retrieving spreadsheet data

--- worker/crons/migration/callback.js
+++ worker/crons/migration/callback.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const path = require('path');
+const util = require('util');
+
+const GoogleSpreadsheet = require('google-spreadsheet');
 
 const { default: log } = require('@financial-times/n-logger');
 
 //const pg = require('../../../db/pg');
+
+const { THE_GOOGLE: { AUTH_FILE_NAME } } = require('config');
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
@@ -18,7 +23,10 @@ module.exports = exports = async () => {
 	running = true;
 
 	try {
+		const key = require(path.resolve(AUTH_FILE_NAME));
+		const ss = await SS('1SjXTysgKVX2bGQtsFP-9pWhrHepKX8qz3ZGHrGiDhtw', key);
 
+		const worksheets = await Promise.all(ss.info.worksheets.map(async item => await item.getRowsAsync()));
 //		const db = await pg();
 
 		log.debug(`${MODULE_ID} | Running migration`);
@@ -33,3 +41,39 @@ module.exports = exports = async () => {
 
 	running = false;
 };
+
+async function SS(id, key) {
+	const ss = new GoogleSpreadsheet(id);
+
+	[
+		'addRow',
+		'addWorksheet',
+		'getCells',
+		'getInfo',
+		'getRows',
+		'makeFeedRequest',
+		'removeWorksheet',
+		'setAuth',
+		'useServiceAccountAuth'
+	].forEach(fn => ss[`${fn}Async`] = util.promisify(ss[fn]));
+
+	await ss.useServiceAccountAuthAsync(key);
+
+	const info = await ss.getInfoAsync();
+
+	[
+		'addRow',
+		'bulkUpdateCells',
+		'clear',
+		'del',
+		'getCells',
+		'getRows',
+		'resize',
+		'setHeaderRow',
+		'setTitle'
+	].forEach(fn =>
+		info.worksheets.forEach(item =>
+			item[`${fn}Async`] = util.promisify(item[fn])));
+
+	return ss;
+}
