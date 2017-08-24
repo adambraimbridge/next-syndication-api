85c68413cc8aa16ebe315529f28f4bc14bc5f83a
constantology <christos@muigui.com>
2017-08-24 14:32:06 +0100

    return a boolean as to whether the key was created to more easily test if the function created the key or skipped it

--- worker/crons/migration/create-key.js
+++ worker/crons/migration/create-key.js
@@ -3,7 +3,6 @@
 const { stat, writeFile } = require('fs');
 const path = require('path');
 const util = require('util');
-
 const { default: log } = require('@financial-times/n-logger');
 
 const { THE_GOOGLE } = require('config');
@@ -13,8 +12,8 @@ const writeFileAsync = util.promisify(writeFile);
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
-module.exports = exports = async function createKey () {
-	const FILE_PATH = path.resolve(THE_GOOGLE.AUTH_FILE_NAME);
+module.exports = exports = async function createKey (AUTH_DATA = THE_GOOGLE) {
+	const FILE_PATH = path.resolve(AUTH_DATA.AUTH_FILE_NAME);
 
 	try {
 		const stat = await statAsync(FILE_PATH);
@@ -22,17 +21,19 @@ module.exports = exports = async function createKey () {
 		if (stat.isFile()) {
 			log.info(`${MODULE_ID} => Key exists: ${FILE_PATH}`);
 
-			return;
+			return false;
 		}
 	}
 	catch (e) {}
 
 	log.info(`${MODULE_ID} => Creating key: ${FILE_PATH}`);
 
-	await writeFileAsync(FILE_PATH, JSON.stringify(THE_GOOGLE.AUTH_KEY, null, 2).replace(/\\\\n/g, '\\n'), {
+	await writeFileAsync(FILE_PATH, JSON.stringify(AUTH_DATA.AUTH_KEY, null, 2).replace(/\\\\n/g, '\\n'), {
 		encoding: 'utf8',
 		mode: '0644'
 	});
 
 	log.info(`${MODULE_ID} => Created key: ${FILE_PATH}`);
+
+	return true;
 };
