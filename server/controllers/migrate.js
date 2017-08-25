57754602012a9893099ea4f3e160195a40825f7f
constantology <christos@muigui.com>
2017-08-25 13:13:22 +0100

    add /migrate endpoint which only I can call!!! 😱

---
+++ server/controllers/migrate.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const path = require('path');
+
+const { default: log } = require('@financial-times/n-logger');
+
+const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
+
+module.exports = exports = async (req, res, next) => {
+	try {
+		if (res.locals.userUuid !== '8ef593a8-eef6-448c-8560-9ca8cdca80a5') {
+
+			res.sendStatus(401);
+
+			return;
+		}
+
+		const migrate = require('../../worker/crons/migration/callback');
+
+		await migrate();
+
+		res.sendStatus(204);
+
+		next();
+	}
+	catch(error) {
+		log.error(`${MODULE_ID}`, {
+			error: error.stack
+		});
+
+		res.sendStatus(500);
+	}
+};
