c9d8632e50b530c6a9e0416b56aa05ae9d6b1f85
constantology <christos@muigui.com>
2017-09-13 17:46:19 +0100

    remove /backup endpoint now we've tested and it works

--- server/app.js
+++ server/app.js
@@ -67,7 +67,7 @@ app.delete(`${BASE_URI_PATH}/save/:content_id`, middleware, require('./controlle
 app.post(`${BASE_URI_PATH}/unsave/:content_id`, middleware, require('./controllers/unsave-by-content-id'));
 app.get(`${BASE_URI_PATH}/user-status`, middleware, require('./controllers/user-status'));
 app.post(`${BASE_URI_PATH}/download-format`, middleware, require('./controllers/update-download-format'));
-app.get(`${BASE_URI_PATH}/backup`, middleware, require('./controllers/backup'));
+//app.get(`${BASE_URI_PATH}/backup`, middleware, require('./controllers/backup'));
 app.get(`${BASE_URI_PATH}/migrate`, middleware, require('./controllers/migrate'));
 
 if (process.env.NODE_ENV !== 'production') {
