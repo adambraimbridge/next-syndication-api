c2b44499dd1475f03b61da55baf28df6ef50a4f2
constantology <christos@muigui.com>
2017-09-13 14:36:01 +0100

    add archiving (zip) to callback  🐿 v2.5.16

--- worker/crons/backup/callback.js
+++ worker/crons/backup/callback.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const { exec } = require('child_process');
-const { stat } = require('fs');
+const { createReadStream, stat } = require('fs');
 const path = require('path');
 const util = require('util');
 
 const { default: log } = require('@financial-times/n-logger');
 
+const archiver = require('archiver');
 const moment = require('moment');
-const { mkdir/*, rm*/ } = require('shelljs');
+const { mkdir, rm } = require('shelljs');
 
 //const pg = require('../../../db/pg');
 
 const {
-	DB
+	DB,
+	DOWNLOAD_ARCHIVE_EXTENSION
 } = require('config');
 
 const execAsync = util.promisify(exec);
@@ -21,22 +23,21 @@ const statAsync = util.promisify(stat);
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
-module.exports = exports = async () => {
-//	const db = await pg();
+module.exports = exports = async (force) => {
+	const START = Date.now();
+	const time = moment().format('YYYY-MM-DD[T]HH');
+	const directory = path.resolve(`./tmp/${time}`);
 
 	try {
-		const time = moment().format('YYYY-MM-DD[T]HH:00');
-		const directory = path.resolve(`./tmp/${time}`);
+		if (force !== true && (await statAsync(directory)).isDirectory()) {
+			log.info(`${MODULE_ID} | THROTTLED!!! Already backed up`);
 
-		try {
-			if ((await statAsync(directory)).isDirectory()) {
-				log.info(`${MODULE_ID} | THROTTLED!!! Already backed up`);
-
-				return;
-			}
+			return;
 		}
-		catch (e) {}
+	}
+	catch (e) {}
 
+	try {
 		mkdir('-p', directory);
 
 		log.info(`${MODULE_ID} | Running backup`);
@@ -73,9 +74,36 @@ module.exports = exports = async () => {
 
 		await execAsync(`${dump_data}`);
 
-		// todo: upload to s3
+		log.info(`${MODULE_ID} | backup complete`);
+
+		const archive = archiver(DOWNLOAD_ARCHIVE_EXTENSION);
+
+		archive.on('error', err => {
+			log.error(`${MODULE_ID} ArchiveError => `, {
+				error: err.stack || err
+			});
+		});
+
+		archive.on('end', () => {
+			log.info(`${MODULE_ID} ArchiveEnd => in ${Date.now() - START}ms`);
+		});
+
+		archive.append(createReadStream(schema_dump_file), { name: path.basename(schema_dump_file) });
+
+		archive.append(createReadStream(data_dump_file), { name: path.basename(data_dump_file) });
+
+		if (archive._state.finalize !== true && archive._state.finalizing !== true) {
+			archive.finalize();
+		}
+
+		return {
+			archive,
+			file_name: `${BACKUP.schema}.${time}.${DOWNLOAD_ARCHIVE_EXTENSION}`
+		};
 	}
 	catch (e) {
 		log.error(`${MODULE_ID} => `, e);
+
+		rm('-rf', directory);
 	}
 };
