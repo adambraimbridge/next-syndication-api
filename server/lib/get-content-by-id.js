0b3a5abeecc2c44e4a1079816ed0e22acd6f1d23
constantology <christos@muigui.com>
2017-09-19 10:15:42 +0100

    use elastic search instead of content api

--- server/lib/get-content-by-id.js
+++ server/lib/get-content-by-id.js
@@ -2,102 +2,113 @@
 
 const path = require('path');
 
-const mime = require('mime-types');
+//const mime = require('mime-types');
 
+const esClient = require('@financial-times/n-es-client');
 const { default: log } = require('@financial-times/n-logger');
 
-const pg = require('../../db/pg');
+//const pg = require('../../db/pg');
 
-const fetchContentById = require('./fetch-content-by-id');
-const formatArticleXML = require('./format-article-xml');
-const getWordCount = require('./get-word-count');
-const decorateArticle = require('./decorate-article');
-const toPlainText = require('./to-plain-text');
+const enrich = require('./enrich');
+//const formatArticleXML = require('./format-article-xml');
+//const getWordCount = require('./get-word-count');
+//const decorateArticle = require('./decorate-article');
+//const toPlainText = require('./to-plain-text');
 
-const isMediaResource = require('../helpers/is-media-resource');
+//const isMediaResource = require('../helpers/is-media-resource');
 
-const {
-	CONTENT_TYPE_ALIAS,
-	DOWNLOAD_ARCHIVE_EXTENSION,
-	DOWNLOAD_ARTICLE_FORMATS,
-	DOWNLOAD_FILENAME_PREFIX
-} = require('config');
-
-const RE_BAD_CHARS = /[^A-Za-z0-9_]/gm;
-const RE_SPACE = /\s/gm;
+//const {
+//	CONTENT_TYPE_ALIAS,
+//	DOWNLOAD_ARCHIVE_EXTENSION,
+//	DOWNLOAD_ARTICLE_FORMATS,
+//	DOWNLOAD_FILENAME_PREFIX
+//} = require('config');
+//
+//const RE_BAD_CHARS = /[^A-Za-z0-9_]/gm;
+//const RE_SPACE = /\s/gm;
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
 module.exports = exports = async (content_id, format) => {
 	const START = Date.now();
 
-	const content = await fetchContentById(content_id);
+	let content = await esClient.get(content_id);
+//	const content = await fetchContentById(content_id);
 
-	if (Object.prototype.toString.call(content) !== '[object Object]' || content instanceof Error) {
+	if (content) {
+		content = enrich(content, format);
+	}
+
+	if (!content) {
+		log.error(`${MODULE_ID} ContentNotFoundError => ${content_id}`);
+
 		return content;
 	}
 
-	content.contentType = content.type.split('/').pop().toLowerCase();
-	content.contentType = CONTENT_TYPE_ALIAS[content.contentType] || content.contentType;
+//	content.contentType = content.type.split('/').pop().toLowerCase();
+//	content.contentType = CONTENT_TYPE_ALIAS[content.contentType] || content.contentType;
+//
+//	if (isMediaResource(content)) {
+//		if (content.transcript) {
+//			if (!content.transcript.startsWith('<body>')) {
+//				content.transcript = `<body>${content.transcript}</body>`;
+//			}
+//
+//			content.__doc = formatArticleXML(content.transcript);
+//
+//			content.__wordCount = content.wordCount = getWordCount(content.__doc);
+//
+//			content.__doc = decorateArticle(content.__doc, content);
+//
+//			content.transcript__CLEAN = content.__doc.toString();
+//
+//			content.transcript__PLAIN = toPlainText(content.transcript__CLEAN);
+//
+//			content.transcriptExtension = DOWNLOAD_ARTICLE_FORMATS[format] || 'docx';
+//		}
+//
+//		content.download = content.dataSource[content.dataSource.length - 1];
+//		content.download.extension = mime.extension(content.download.mediaType);
+//		content.extension = DOWNLOAD_ARCHIVE_EXTENSION;
+//	}
+//	else if (content.bodyXML) {
+//		content.extension = DOWNLOAD_ARTICLE_FORMATS[format] || 'docx';
+//
+//		content.__doc = formatArticleXML(content.bodyXML);
+//
+//		content.__wordCount = content.wordCount = getWordCount(content.__doc);
+//
+//		content.__doc = decorateArticle(content.__doc, content);
+//
+//		content.bodyXML__CLEAN = content.__doc.toString();
+//
+//		if (content.extension === 'plain') {
+//			// we need to strip all formatting — leaving only paragraphs — and pass this to pandoc for plain text
+//			// otherwise it will uppercase the whole article title and anything bold, as well as leave other weird
+//			// formatting in the text file
+//			content.bodyXML__PLAIN = toPlainText(content.__doc.toString());
+//		}
+//	}
+//
+//	content.fileName = DOWNLOAD_FILENAME_PREFIX + content.title.replace(RE_SPACE, '_').replace(RE_BAD_CHARS, '').substring(0, 12);
 
-	if (isMediaResource(content)) {
-		if (content.transcript) {
-			if (!content.transcript.startsWith('<body>')) {
-				content.transcript = `<body>${content.transcript}</body>`;
-			}
+	log.debug(`${MODULE_ID} GetContentSuccess =>-2 ${content.content_id} in ${Date.now() - START}ms`);
 
-			content.__doc = formatArticleXML(content.transcript);
+//	process.nextTick(async () => {
+//		const db = await pg();
 
-			content.__wordCount = content.wordCount = getWordCount(content.__doc);
+//		let data = Object.assign({}, content);
+//		const data = JSON.parse(JSON.stringify(content));
 
-			content.__doc = decorateArticle(content.__doc, content);
+//		delete data.document;
 
-			content.transcript__CLEAN = content.__doc.toString();
+//		delete data.__doc;
+//		delete data.extension;
+//		delete data.download;
+//		delete data.transcriptExtension;
 
-			content.transcript__PLAIN = toPlainText(content.transcript__CLEAN);
-
-			content.transcriptExtension = DOWNLOAD_ARTICLE_FORMATS[format] || 'docx';
-		}
-
-		content.download = content.dataSource[content.dataSource.length - 1];
-		content.download.extension = mime.extension(content.download.mediaType);
-		content.extension = DOWNLOAD_ARCHIVE_EXTENSION;
-	}
-	else if (content.bodyXML) {
-		content.extension = DOWNLOAD_ARTICLE_FORMATS[format] || 'docx';
-
-		content.__doc = formatArticleXML(content.bodyXML);
-
-		content.__wordCount = content.wordCount = getWordCount(content.__doc);
-
-		content.__doc = decorateArticle(content.__doc, content);
-
-		content.bodyXML__CLEAN = content.__doc.toString();
-
-		if (content.extension === 'plain') {
-			// we need to strip all formatting — leaving only paragraphs — and pass this to pandoc for plain text
-			// otherwise it will uppercase the whole article title and anything bold, as well as leave other weird
-			// formatting in the text file
-			content.bodyXML__PLAIN = toPlainText(content.__doc.toString());
-		}
-	}
-
-	content.fileName = DOWNLOAD_FILENAME_PREFIX + content.title.replace(RE_SPACE, '_').replace(RE_BAD_CHARS, '').substring(0, 12);
-
-	log.debug(`${MODULE_ID} GetContentSuccess => ${content.id} in ${Date.now() - START}ms`);
-
-	process.nextTick(async () => {
-		const db = await pg();
-
-		let data = Object.assign({}, content);
-
-		delete data.__doc;
-		delete data.extension;
-		delete data.download;
-		delete data.transcriptExtension;
-
-		await db.syndication.upsert_content([data.id.split('/').pop(), data.contentType, data]);
-	});
+//		await db.syndication.upsert_content([data.content_id, data.content_type, data]);
+//	});
 
 	return content;
 };
--- server/lib/get-content.js
+++ server/lib/get-content.js
@@ -2,37 +2,49 @@
 
 const path = require('path');
 
+const esClient = require('@financial-times/n-es-client');
 const { default: log } = require('@financial-times/n-logger');
 
-const pg = require('../../db/pg');
+//const pg = require('../../db/pg');
 
-const getContentById = require('./get-content-by-id');
+const enrich = require('./enrich');
+//const getContentById = require('./get-content-by-id');
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
 module.exports = exports = async (DISTINCT_ITEMS) => {
-	const db = await pg();
+	const START = Date.now();
+
+//	const db = await pg();
 
 	const DISTINCT_ITEMS_LENGTH = DISTINCT_ITEMS.length;
 
-	let cachedItems = await db.run(`SELECT * FROM syndication.get_content(ARRAY[$text$${DISTINCT_ITEMS.join('$text$, $text$')}$text$]);`);
+//	let cachedItems = await db.run(`SELECT * FROM syndication.get_content(ARRAY[$text$${DISTINCT_ITEMS.join('$text$, $text$')}$text$]);`);
 
-	log.info(`${MODULE_ID} => ${cachedItems.length} items retrieved from cache`);
+//	log.info(`${MODULE_ID} => ${cachedItems.length} items retrieved from cache`);
 
-	cachedItems = cachedItems.map(item => item.data);
+//	cachedItems = cachedItems.map(item => item.data);
 
-	cachedItems.forEach(item =>
-		DISTINCT_ITEMS.splice(DISTINCT_ITEMS.indexOf(item.id.split('/').pop()), 1));
+//	cachedItems.forEach(item =>
+//		DISTINCT_ITEMS.splice(DISTINCT_ITEMS.indexOf(item.id.split('/').pop()), 1));
 
-	let contentItems = (await Promise.all(DISTINCT_ITEMS.map(async content_id => await getContentById(content_id))));
+	let esContentItems = await esClient.mget({
+		ids: DISTINCT_ITEMS
+	});
 
-	contentItems = contentItems.filter(item => Object.prototype.toString.call(item) === '[object Object]');
+	log.info(`${MODULE_ID} => ${esContentItems.length} items retrieved from elastic search api in ${Date.now() - START}ms`);
 
-	log.info(`${MODULE_ID} => ${contentItems.length} items retrieved from content api`);
+	esContentItems = esContentItems.filter(item => enrich(item));
 
-	const items = contentItems.concat(cachedItems);
+//	console.log(esContentItems);
 
-	log.info(`${MODULE_ID} => ${items.length}/${DISTINCT_ITEMS_LENGTH} items found`);
+//	let contentItems = (await Promise.all(DISTINCT_ITEMS.map(async content_id => await getContentById(content_id))));
+//
+//	contentItems = contentItems.filter(item => Object.prototype.toString.call(item) === '[object Object]');
 
-	return items;
+//	const items = contentItems.concat(cachedItems);
+
+	log.info(`${MODULE_ID} => ${esContentItems.length}/${DISTINCT_ITEMS_LENGTH} items found in ${Date.now() - START}ms`);
+
+	return esContentItems;
 };
