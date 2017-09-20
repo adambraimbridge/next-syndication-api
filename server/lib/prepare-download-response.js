217293e6012277ab0f3955216556a90a7a8dfcd5
constantology <christos@muigui.com>
2017-09-20 12:19:45 +0100

    passing content to function now we're doing things differently

--- server/lib/prepare-download-response.js
+++ server/lib/prepare-download-response.js
@@ -2,9 +2,7 @@
 
 const { DOWNLOAD_ARTICLE_EXTENSION_OVERRIDES } = require('config');
 
-module.exports = exports = (res) => {
-	let { __content: content } = res;
-
+module.exports = exports = (res, content) => {
 	let extension = DOWNLOAD_ARTICLE_EXTENSION_OVERRIDES[content.extension] || content.extension;
 
 	res.attachment(`${content.fileName}.${extension}`);
