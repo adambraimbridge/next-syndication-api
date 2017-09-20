1f9390e2b8be643977f218b212d4e43218350d01
constantology <christos@muigui.com>
2017-09-20 15:23:35 +0100

    fix b0rken tests

--- test/server/lib/prepare-download-response.spec.js
+++ test/server/lib/prepare-download-response.spec.js
@@ -30,23 +30,23 @@ describe(MODULE_ID, function () {
 	});
 
 	it('uses the configured override when `content.extension` is not a valid extension type', function () {
-		let content = res.__content = {
+		let content = {
 			extension: 'plain',
 			fileName: 'some_awesome_article'
 		};
 
-		underTest(res);
+		underTest(res, content);
 
 		expect(res.attachment).calledWith(`${content.fileName}.${DOWNLOAD_ARTICLE_EXTENSION_OVERRIDES[content.extension]}`);
 	});
 
 	it('uses the `content.extension` when no override is configured', function () {
-		let content = res.__content = {
+		let content = {
 			extension: 'docx',
 			fileName: 'some_awesome_article'
 		};
 
-		underTest(res);
+		underTest(res, content);
 
 		expect(res.attachment).calledWith(`${content.fileName}.${content.extension}`);
 	});
