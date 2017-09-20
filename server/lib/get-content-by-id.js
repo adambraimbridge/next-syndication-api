381c8139d4e4e11d6f0b79055ee8cbf4dde8371a
constantology <christos@muigui.com>
2017-09-20 12:48:47 +0100

    remove old code

--- server/lib/get-content-by-id.js
+++ server/lib/get-content-by-id.js
@@ -2,30 +2,10 @@
 
 const path = require('path');
 
-//const mime = require('mime-types');
-
 const esClient = require('@financial-times/n-es-client');
 const { default: log } = require('@financial-times/n-logger');
 
-//const pg = require('../../db/pg');
-
 const enrich = require('./enrich');
-//const formatArticleXML = require('./format-article-xml');
-//const getWordCount = require('./get-word-count');
-//const decorateArticle = require('./decorate-article');
-//const toPlainText = require('./to-plain-text');
-
-//const isMediaResource = require('../helpers/is-media-resource');
-
-//const {
-//	CONTENT_TYPE_ALIAS,
-//	DOWNLOAD_ARCHIVE_EXTENSION,
-//	DOWNLOAD_ARTICLE_FORMATS,
-//	DOWNLOAD_FILENAME_PREFIX
-//} = require('config');
-//
-//const RE_BAD_CHARS = /[^A-Za-z0-9_]/gm;
-//const RE_SPACE = /\s/gm;
 
 const MODULE_ID = path.relative(process.cwd(), module.id) || require(path.resolve('./package.json')).name;
 
