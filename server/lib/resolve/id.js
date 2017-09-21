03b59e58809714b0676dc923907b4202405bbc94
constantology <christos@muigui.com>
2017-09-21 14:23:24 +0100

    fix issue

--- server/lib/resolve/id.js
+++ server/lib/resolve/id.js
@@ -3,4 +3,10 @@
 const path = require('path');
 const url = require('url');
 
-module.exports = exports = item => path.basename((url.parse(item)).pathname);
+module.exports = exports = item => {
+	if (item.startsWith('http')) {
+		return path.basename((url.parse(item)).pathname);
+	}
+
+	return item;
+};
