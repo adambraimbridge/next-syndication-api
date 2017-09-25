c0410d56b533398a9682e91347308abc7d61a6d7
constantology <christos@muigui.com>
2017-09-25 16:35:00 +0100

    fix incorrect check for notAvailable

--- server/controllers/history.js
+++ server/controllers/history.js
@@ -64,9 +64,9 @@ module.exports = exports = async (req, res, next) => {
 		history.items = history.items.map(item => RESOLVE_PROPERTIES.reduce((acc, prop) => {
 			let contentItem = contentItemsMap[item.content_id] || contentItemsMap[item.id] || {};
 
-			acc[prop] = resolve[prop](contentItem[prop], prop, contentItem, item, CONTRACT);
+			acc[prop] = resolve[prop](contentItem[prop] || item[prop], prop, contentItem, item, CONTRACT);
 
-			if (!contentItem) {
+			if (!contentItemsMap[item.content_id] && !contentItemsMap[item.id]) {
 				acc.notAvailable = true;
 			}
 
