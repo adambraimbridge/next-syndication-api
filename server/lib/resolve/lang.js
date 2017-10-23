342d8b6c84f4929003ef61a9a067b424996e7a8d
constantology <christos@muigui.com>
2017-10-23 14:36:40 +0100

    if no existing item, also default to `wn`

--- server/lib/resolve/lang.js
+++ server/lib/resolve/lang.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = exports = (val, prop, item, existing) => existing.lang || existing.iso_lang_code || 'en';
+module.exports = exports = (val, prop, item, existing) => existing ? existing.lang || existing.iso_lang_code || 'en' : 'en';
