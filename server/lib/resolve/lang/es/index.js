e824a12c2b341eeb29c39f98cebf08ebc13334b0
constantology <christos@muigui.com>
2017-10-23 17:15:40 +0100

    add previewText to /translations

--- server/lib/resolve/lang/es/index.js
+++ server/lib/resolve/lang/es/index.js
@@ -3,5 +3,6 @@
 exports.byline = require('./byline');
 exports.content_area = require('./content_area');
 exports.lang = require('./lang');
+exports.previewText = require('./previewText');
 exports.translatedDate = require('./translatedDate');
 exports.translatedDateDisplay = require('./translatedDateDisplay');
---
+++ server/lib/resolve/lang/es/previewText.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = exports = (val, prop, item) => {
+	const previewText = item.document.childNodes[0].childNodes[1].textContent;
+
+	return previewText.length <= 105 ? previewText : (previewText.substring(0, 105) + '...');
+};
