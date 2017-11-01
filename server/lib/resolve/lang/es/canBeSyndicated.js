b3d989caac8700072a98f127227c5010879a6caf
constantology <christos@muigui.com>
2017-11-01 12:20:16 +0000

    updated to return `verify` when a user has not got contractual rights to download this type of content

--- server/lib/resolve/lang/es/canBeSyndicated.js
+++ server/lib/resolve/lang/es/canBeSyndicated.js
@@ -1,3 +1,16 @@
 'use strict';
 
-module.exports = exports = () => 'yes';
+module.exports = exports = (val, prop, item = {}, existing = {}, contract) => {
+	const content_area = Object.prototype.hasOwnProperty.call(item, 'content_area')
+						? item.content_area.toLowerCase()
+						: Object.prototype.hasOwnProperty.call(existing, 'content_area')
+						? existing.content_area.toLowerCase()
+						: null;
+
+	if ((content_area === 'spanish content' && contract.allowed.spanish_content)
+	|| (content_area === 'spanish weekend' && contract.allowed.spanish_weekend)) {
+		return 'yes';
+	}
+
+	return 'verify';
+};
