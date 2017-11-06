ca521758efa3955ea04f9395d64a80731153ee11
constantology <christos@muigui.com>
2017-11-06 12:19:45 +0000

    updated canBeSyndicated to support new podcast rules

--- server/lib/resolve/canBeSyndicated.js
+++ server/lib/resolve/canBeSyndicated.js
@@ -1,3 +1,19 @@
 'use strict';
 
-module.exports = exports = item => item;
+const type = require('./type');
+
+module.exports = exports = (val, prop, item, existing, contract) => {
+	let content_type = item.content_type || (existing || {}).content_type || type(item.type, 'type', item, existing, contract);
+
+	if (content_type === 'podcast') {
+		if (contract && contract.itemsMap) {
+			const asset = contract.itemsMap[content_type];
+
+			if (asset && asset.download_limit > 0) {
+				return 'yes';
+			}
+		}
+	}
+
+	return val;
+};
--- test/server/lib/resolve/canBeSyndicated.spec.js
+++ test/server/lib/resolve/canBeSyndicated.spec.js
@@ -11,15 +11,47 @@ const underTest = require('../../../../server/lib/resolve/canBeSyndicated');
 const MODULE_ID = path.relative(`${process.cwd()}/test`, module.id) || require(path.resolve('./package.json')).name;
 
 describe(MODULE_ID, function () {
-	it('returns its first argument', function() {
-		expect(underTest('yes')).to.equal('yes');
+	describe('if `content.type` is not equal to `podcast`', function () {
+		it('returns its first argument', function() {
+			expect(underTest('yes', 'canBeSyndicated', { content_type: 'article' })).to.equal('yes');
 
-		expect(underTest('no')).to.equal('no');
+			expect(underTest('no', 'canBeSyndicated', { content_type: 'video' })).to.equal('no');
 
-		expect(underTest('verify')).to.equal('verify');
+			expect(underTest('verify', 'canBeSyndicated', { content_type: 'article' })).to.equal('verify');
 
-		expect(underTest(null)).to.equal(null);
+			expect(underTest(null, 'canBeSyndicated', { content_type: 'video' })).to.equal(null);
 
-		expect(underTest('null')).to.equal('null');
+			expect(underTest('null', 'canBeSyndicated', { content_type: 'article' })).to.equal('null');
+		});
+	});
+
+	describe('if `content.type` is equal to `podcast`', function () {
+		describe('if the contract can NOT download podcasts', function() {
+			it('returns its first argument', function() {
+				expect(underTest('yes', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 0 } } })).to.equal('yes');
+
+				expect(underTest('no', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { } })).to.equal('no');
+
+				expect(underTest('verify', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 0 } } })).to.equal('verify');
+
+				expect(underTest(null, 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { } })).to.equal(null);
+
+				expect(underTest('null', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 0 } } })).to.equal('null');
+			});
+		});
+
+		describe('if the contract can download podcasts', function() {
+			it('returns `yes`', function() {
+				expect(underTest('yes', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 1 } } })).to.equal('yes');
+
+				expect(underTest('no', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 10 } } })).to.equal('yes');
+
+				expect(underTest('verify', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 100 } } })).to.equal('yes');
+
+				expect(underTest(null, 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 32 } } })).to.equal('yes');
+
+				expect(underTest('null', 'canBeSyndicated', { content_type: 'podcast'}, {}, { itemsMap: { podcast: { download_limit: 64 } } })).to.equal('yes');
+			});
+		});
 	});
 });
