e54eceb9336504290326328d5770f5c27056b386
Simon Legg <leggsimon@gmail.com>
2017-12-08 16:32:37 +0000

    get content_area from db for spanish (#52)
    🐿 v2.5.16

--- server/lib/get-history-by-contract-id.js
+++ server/lib/get-history-by-contract-id.js
@@ -47,6 +47,12 @@ module.exports = exports = async ({ contract_id, limit, offset, type, user_id })
 	const total = parseInt(totalRes.count, 10);
 	const allExisting = await getAllExistingItemsForContract(contract_id);
 
+	await items.filter(item => item.iso_lang_code === 'es').forEach(async item => {
+		const query = `SELECT * FROM syndication.get_content_es_by_id($text$${item.content_id}$text$)`;
+		const [content] = await db.run(query);
+		item.content_area = content.content_area;
+	});
+
 	items.forEach(item => {
 		const existing = allExisting[item.content_id];
 
