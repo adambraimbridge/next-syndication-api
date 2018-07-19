c5f7cffafb5a391741a1351b91ee31f45aeaa7c7
dapenguin <anthony.jeffery@littleblueweb.com>
2018-07-19 14:11:09 +0100

    Separate queries into different functions  🐿 v2.10.0

--- server/controllers/erasure-search.js
+++ server/controllers/erasure-search.js
@@ -1,7 +1,37 @@
 const { default: log } = require('@financial-times/n-logger');
+const operation = 'erasure-search';
+
+async function searchUserTableByUuid (db, uuid, requestedBy) {
+	try {
+		const userTableResults = await db.run(`SELECT * FROM syndication.users WHERE user_id = '${uuid}';`);
+		log.info({ operation, subOp: 'search-user-table-by-uuid', requestedBy, uuid, userTableResults});
+		return { uuid, table: 'users', recordsFound: userTableResults.length };
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function searchUserTableByEmail (db, email, requestedBy) {
+	try {
+		const userTableResults = await db.run(`SELECT * FROM syndication.users WHERE email = '${email}';`);
+		log.info({ operation, subOp: 'search-user-table-by-email', requestedBy, email, userTableResults});
+		return { email, table: 'users', recordsFound: userTableResults.length };
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function searchMigratedUsersTableByUuid (db, uuid, requestedBy) {
+	try {
+		const migratedUserTableResults = await db.run(`SELECT * FROM syndication.migrated_users WHERE user_id = '${uuid}';`);
+		log.info({ operation, subOp: 'search-migrated-user-table-by-uuid', requestedBy, uuid, migratedUserTableResults});
+		return { uuid, table: 'migrated_users', recordsFound: migratedUserTableResults.length };
+	} catch (error) {
+		throw error;
+	}
+}
 
 module.exports = async (req, res, next) => {
-	const operation = 'erasure-search';
 	const requestedBy = req.cookies.s3o_username;
 	const db = res.locals.$DB;
 	const { email, uuid } = req.body;
@@ -14,22 +44,17 @@ module.exports = async (req, res, next) => {
 		if (uuid) {
 			const uuids = uuid.split(/\r?\n/);
 			await Promise.all(uuids.map(async (uuid) => {
-				const userTableResults = await db.run(`SELECT * FROM syndication.users WHERE user_id = '${uuid}';`);
-				log.info({ operation, subOp: 'search-user-table-by-uuid', requestedBy, uuid, userTableResults});
-				uuidSearchResults.push({ uuid, table: 'users', recordsFound: userTableResults.length });
-
-				const migratedUserTableResults = await db.run(`SELECT * FROM syndication.migrated_users WHERE user_id = '${uuid}';`);
-				log.info({ operation, subOp: 'search-migrated-user-table-by-uuid', requestedBy, uuid, migratedUserTableResults});
-				uuidSearchResults.push({ uuid, table: 'migrated_users', recordsFound: migratedUserTableResults.length });
+				const userTableResults = await searchUserTableByUuid(db, uuid, requestedBy);
+				const migratedUserTableResults = await searchMigratedUsersTableByUuid(db, uuid, requestedBy);
+				uuidSearchResults.push(userTableResults, migratedUserTableResults);
 			}));
 		}
 
 		if (email) {
 			const emails = email.split(/\r?\n/);
 			await Promise.all(emails.map(async (email) => {
-				const userTableResults = await db.run(`SELECT * FROM syndication.users WHERE email = '${email}';`);
-				log.info({ operation, subOp: 'search-user-table-by-email', requestedBy, email, userTableResults});
-				emailSearchResults.push({ email, table: 'users', recordsFound: userTableResults.length });
+				const userTableResults = await searchUserTableByEmail(db, email, requestedBy);
+				emailSearchResults.push(userTableResults);
 			}));
 		}
 
