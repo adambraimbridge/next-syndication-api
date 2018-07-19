62f414e26d9eda11d42e5f3a14d5b818d16616c5
dapenguin <anthony.jeffery@littleblueweb.com>
2018-07-19 11:46:23 +0100

    Remove commented code  🐿 v2.10.0

--- server/controllers/erasure.js
+++ server/controllers/erasure.js
@@ -1,6 +1,3 @@
 module.exports = (req, res) => {
-    // const username = req.cookies.s3o_username;
-    // const viewModel = { username };
-    // res.render('erasure', { viewModel });
     res.render('erasure');
 };
