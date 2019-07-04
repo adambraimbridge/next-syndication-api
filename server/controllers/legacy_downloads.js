411043fb1419a2c7b010abcec3ed958cbb93e194
Alex Curtis <alex.curtis@ft.com>
2019-07-04 08:36:30 +0100

    Revert "Merge pull request #147 from Financial-Times/@maxcbc/remove-superdooperuser"
    This reverts commit 98818485886f6cfc5378465d343a912c42423af1, reversing
    changes made to e00fe14ece3d1b093aa3aef3d219ed42f9814a63.

---
+++ server/controllers/legacy_downloads.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const { stat } = require('fs');
+const util = require('util');
+
+const log = require('../lib/logger');
+const createKey = require('../../worker/create-key');
+const { THE_GOOGLE: { AUTH_FILE_NAME } } = require('config');
+const legacy_downloads = require('../../worker/crons/legacy_downloads/callback');
+const statAsync = util.promisify(stat);
+
+const ACL = {
+	user: false,
+	superuser: false,
+	superdooperuser: true
+};
+
+module.exports = exports = async (req, res, next) => {
+	try {
+		const { locals: { user } } = res;
+
+		if (!user || ACL[user.role] !== true) {
+			res.sendStatus(401);
+
+			return;
+		}
+
+		let createAuthKey = false;
+
+		try {
+			await statAsync(AUTH_FILE_NAME);
+
+			if (!stat.isFile()) {
+				createAuthKey = true;
+			}
+		} catch (e) {
+			createAuthKey = true;
+		}
+
+		if (createAuthKey === true) {
+
+
+			await createKey();
+		}
+
+
+		const val = await legacy_downloads(true);
+
+		if (val) {
+			res.status(200);
+
+			res.json(val);
+		}
+		else {
+			res.sendStatus(204);
+		}
+
+		next();
+	}
+	catch(error) {
+		log.error({
+			event: 'LEGACY_DOWNLOADS_ERROR',
+			error
+		});
+
+		res.sendStatus(500);
+	}
+};
