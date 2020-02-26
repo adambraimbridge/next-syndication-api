190c1aca7d6ba434ea0cb502fead346f1f933087
Maggie Allen <maggie.allen@ft.com>
2020-02-26 12:40:28 +0000

    adds a samplePeriod parameter to the 401 reponses healthcheck 🐿 v2.12.5

--- health/error-spikes.js
+++ health/error-spikes.js
@@ -4,9 +4,10 @@ module.exports = nHealth.runCheck({
 	type: 'graphiteThreshold',
 	metric: 'divideSeries(sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.401.count),sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.*.count))',
 	threshold: 0.10,
+	samplePeriod: '15min',
 	name: '401 rate for articles is acceptable',
 	severity: 1,
-	businessImpact: 'Error rate for the syndication-api is higher than the acceptable threshold of 10%.',
+	businessImpact: 'Error rate for the syndication-api has exceeded a threshold of 10% over the last 15 minutes.',
 	technicalSummary: 'The proportion of 401 (Unauthorized) responses for syndication API requests across all heroku dynos vs all responses is higher than a threshold of 0.10',
 	panicGuide: 'Check the heroku logs for the app for any error messages. Possible causes could be incorrect data from Salesforce or Membership’s ALS'
 })
