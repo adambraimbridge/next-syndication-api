d5e635594c989be0c9793832caf95e58b56d4720
Alessandro Vito <alessandro.vito84@gmail.com>
2020-03-17 17:35:10 +0000

    NOPSCOSP-224 Increase the sample period to 20 minutes
    ## Meta
    See similar PR #188 
    Jira [NOPSCOPS-224](https://financialtimes.atlassian.net/browse/NOPSCOPS-224)

--- health/error-spikes.js
+++ health/error-spikes.js
@@ -4,7 +4,7 @@ module.exports = nHealth.runCheck({
 	type: 'graphiteThreshold',
 	metric: 'divideSeries(sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.401.count),sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.*.count))',
 	threshold: 0.10,
-	samplePeriod: '15min',
+	samplePeriod: '20min',
 	name: '401 rate for articles is acceptable',
 	severity: 1,
 	businessImpact: 'Error rate for the syndication-api has exceeded a threshold of 10% over the last 15 minutes.',
