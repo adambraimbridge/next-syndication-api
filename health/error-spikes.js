6c564f632f3d7d321b68ea4a70ef080b54e9893e
alevito <alessandro.vito84@gmail.com>
2020-03-20 12:22:43 +0000

    NOPSCOPS-224 Syndication-API 401 alert increase threshold and sample period * increased threshold from 0.10 to 0.25 * increased sample period from 20 to 60 minutes

--- health/error-spikes.js
+++ health/error-spikes.js
@@ -1,13 +1,16 @@
 const nHealth = require('n-health');
+const statusCode = 401;
+const threshold = 0.25;
+const samplePeriod = 60; // minutes
 
 module.exports = nHealth.runCheck({
 	type: 'graphiteThreshold',
-	metric: 'divideSeries(sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.401.count),sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.*.count))',
-	threshold: 0.10,
-	samplePeriod: '20min',
-	name: '401 rate for articles is acceptable',
+	metric: `divideSeries(sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.${statusCode}.count),sumSeries(next.heroku.syndication-api.web_*.express.default_route_GET.res.status.*.count))`,
+	threshold,
+	samplePeriod: `${samplePeriod}min`,
+	name: `${statusCode} rate for articles is acceptable`,
 	severity: 1,
-	businessImpact: 'Error rate for the syndication-api has exceeded a threshold of 10% over the last 20 minutes.',
-	technicalSummary: 'The proportion of 401 (Unauthorized) responses for syndication API requests across all heroku dynos vs all responses is higher than a threshold of 0.10 over the last 20 minutes',
+	businessImpact: `Error rate for the syndication-api has exceeded a threshold of ${threshold * 100}% over the last ${samplePeriod} minutes.`,
+	technicalSummary: `The proportion of ${statusCode} (Unauthorized) responses for syndication API requests across all heroku dynos vs all responses is higher than a threshold of ${threshold} over the last ${samplePeriod} minutes`,
 	panicGuide: 'Check the heroku logs for the app for any error messages. Possible causes could be incorrect data from Salesforce or Membership’s ALS'
 })
