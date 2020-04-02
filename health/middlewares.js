8fc42780de8ec0d3461061d732af190a3fc91960
John Flockton <j.r.flockton@gmail.com>
2020-04-02 10:42:13 +0100

    Small stylistic changes 🐿 v2.12.6

--- health/middlewares.js
+++ health/middlewares.js
@@ -5,7 +5,7 @@ const { HEALTCHECK_DUMMY_USER_UUID } = require('config');
 const panicGuide = 'Contact the FT Accounts team #ft-accounts-team.';
 const businessImpact = 'Users will be unable to use the Syndication apps';
 
-const middlewares = [
+const services = [
 	{
 		name: 'Syndication user rights reachable',
 		url: 'https://session-next.ft.com/products',
@@ -16,25 +16,25 @@ const middlewares = [
 		name: 'Syndication licence reachable',
 		url: `https://api.ft.com/licences?userid=${HEALTCHECK_DUMMY_USER_UUID}`,
 		technicalSummary:
-			'This middleware does the GET the syndication licence details by passing in the unique User ID.'
+			'Endpoint the syndication licence details by passing in a unique User ID.'
 	},
 	{
 		name: 'User profile reachable',
 		url: `https://api.ft.com/users/${HEALTCHECK_DUMMY_USER_UUID}/profile/basic`,
 		technicalSummary:
-			'This middleware does the GET to get the user profile details by passing the unique User ID.'
+			'Endpoint to get the user profile details by passing a unique User ID.'
 	},
 	{
 		name: 'User authentication reachable',
 		url: 'https://api.ft.com/authorize',
 		technicalSummary:
-			'This middleware does the GET to get token required to view a user\'s profile details.'
+			'Endpoint to get token required to view a user\'s profile details.'
 	}
 ];
 
-module.exports = middlewares.map(middleware =>
+module.exports = services.map(svc =>
 	nHealth.runCheck(
-		Object.assign({}, middleware, {
+		Object.assign({}, svc, {
 			businessImpact,
 			panicGuide,
 			severity: 1,
